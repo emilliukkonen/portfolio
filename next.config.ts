dde71c03f4df507a1624e1e5325ea51f823b1a62
Emil <emil.liukkonen@gmail.com>
2025-06-15 15:38:36 +0300

    changes to nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
-  // optionally set base path if deploying to github pages repo (usually username.github.io/repo)
-  // basePath: '/your-repo-name',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // required for next/image to work with static export
+  },
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true, // disables Next.js Image Optimization for static export
-  },
-};
